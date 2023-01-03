b5b12a3886ee4704fab6372a168cb2fa21832601
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-01-03 10:08:59 +0500

    change js to vue.js

--- building-first-app/app.js
+++ building-first-app/app.js
@@ -1,12 +1,26 @@
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+    },
+  },
+}).mount("#app");
 
-function addGoal() {
-    const text = input.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = text;
-    listEl.appendChild(listItemEl);
-}
+// const button = document.querySelector('button');
+// const input = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-button.addEventListener('click', addGoal)
+// function addGoal() {
+//     const text = input.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = text;
+//     listEl.appendChild(listItemEl);
+// }
+
+// button.addEventListener('click', addGoal)
